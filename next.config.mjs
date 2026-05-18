fa0b13a5bbdaac688a06258007a10b778da0812b
hssanmoncef2-gif <hssanmoncef2@gmail.com>
2026-05-18 22:44:44 +0100

    fix design 1

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typedRoutes: true,
   images: {
     domains: ['your-supabase-project.supabase.co'],
     formats: ['image/avif', 'image/webp'],
